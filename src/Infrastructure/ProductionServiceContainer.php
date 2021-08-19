e1591987cba4a5f5365cfc4296263ee27a69327a
zero <teclaizerai@gmail.com>
2021-08-19 14:17:50 +0200

    Added ProductionServiceContainer class (factory for badge application).

---
+++ src/Infrastructure/ProductionServiceContainer.php
@@ -0,0 +1,64 @@
+<?php declare(strict_types=1);
+
+namespace App\Infrastructure;
+
+use Badge\Infrastructure\ServiceContainer;
+use Bitbucket\Client as BitbucketClient;
+use Github\Client as GithubClient;
+use GuzzleHttp\Client;
+use GuzzleHttp\ClientInterface as GuzzleHttpClient;
+use Packagist\Api\Client as packagistClient;
+
+class ProductionServiceContainer extends ServiceContainer
+{
+    protected ?GithubClient $githubClient = null;
+
+    protected ?BitbucketClient $bitbucketClient = null;
+
+    protected ?PackagistClient $packagistClient = null;
+
+    protected ?GuzzleHttpClient $httpClient = null;
+
+    public function __construct(GithubClient $githubClient = null, BitbucketClient $bitbucketClient = null, PackagistClient $packagistClient = null)
+    {
+        $this->githubClient = $githubClient ?? $githubClient;
+        $this->bitbucketClient = $bitbucketClient ?? $bitbucketClient;
+        $this->packagistClient = $packagistClient ?? $packagistClient;
+    }
+
+    protected function packagistApiClient(): PackagistClient
+    {
+        if ($this->packagistClient === null) {
+            $this->packagistClient = new packagistClient();
+        }
+
+        return $this->packagistClient;
+    }
+
+    protected function githubApiClient(): GithubClient
+    {
+        if ($this->githubClient === null) {
+            $this->githubClient = new GithubClient();
+        }
+
+        return $this->githubClient;
+    }
+
+    protected function bitbucketApiClient(): BitbucketClient
+    {
+        if ($this->bitbucketClient === null) {
+            $this->bitbucketClient = new BitbucketClient();
+        }
+
+        return $this->bitbucketClient;
+    }
+
+    protected function httpClient(): GuzzleHttpClient
+    {
+        if ($this->httpClient === null) {
+            $this->httpClient = new Client();
+        }
+
+        return $this->httpClient;
+    }
+}
