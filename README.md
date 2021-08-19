e1542c3f90525f514f5b9ee128d0094a00bc8210
zero <teclaizerai@gmail.com>
2021-08-19 21:57:16 +0200

    Added Suggesters useCase.

--- README.md
+++ README.md
@@ -41,3 +41,7 @@ http://127.0.0.1/doctrine/collections/
 ### Dependents badge:
 
 http://127.0.0.1/doctrine/collections/dependents
+
+### Suggesters badge:
+
+http://127.0.0.1/nyholm/psr7/suggesters
---
+++ src/Controller/SuggestersController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use App\Infrastructure\BadgeResponseFactory;
+use Badge\Application\PortIn\CreateSuggestersBadge;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class SuggestersController extends AbstractController
+{
+    private CreateSuggestersBadge $useCase;
+
+    private BadgeResponseFactory  $responseFactory;
+
+    public function __construct(CreateSuggestersBadge $useCase, BadgeResponseFactory $responseFactory)
+    {
+        $this->useCase = $useCase;
+
+        $this->responseFactory = $responseFactory;
+    }
+
+    public function __invoke(Request $request): Response
+    {
+        $repository = $request->get('repository');
+
+        $image = $this->useCase->createSuggestersBadge($repository);
+
+        return $this->responseFactory->createFromImage($image, 200);
+    }
+}
