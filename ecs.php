a3ad3397dedab36f556f35a9bf1a71ee5791efe5
zero <teclaizerai@gmail.com>
2021-08-20 13:45:51 +0200

    [Tools] update ecs configuration.

--- ecs.php
+++ ecs.php
@@ -1,6 +1,11 @@
 <?php declare(strict_types=1);
 
 use PhpCsFixer\Fixer\ArrayNotation\ArraySyntaxFixer;
+use PhpCsFixer\Fixer\FunctionNotation\NativeFunctionInvocationFixer;
+use PhpCsFixer\Fixer\Import\NoUnusedImportsFixer;
+use PhpCsFixer\Fixer\NamespaceNotation\BlankLineAfterNamespaceFixer;
+use PhpCsFixer\Fixer\Strict\DeclareStrictTypesFixer;
+use PhpCsFixer\Fixer\Strict\StrictComparisonFixer;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symplify\EasyCodingStandard\ValueObject\Option;
 use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
@@ -13,20 +18,31 @@ return static function (ContainerConfigurator $containerConfigurator): void {
         //__DIR__ . '/tests'
     ]);
     $parameters->set(Option::SKIP, [
-        // skip paths with legacy code
         __DIR__ . '/src/Kernel.php',
+
         // ignore specific error message
         #'Cognitive complexity for method "addAction" is 13 but has to be less than or equal to 8.',
     ]);
 
-    // A. standalone rule
+
     $services = $containerConfigurator->services();
+
+    $services->set(DeclareStrictTypesFixer::class);
+
+    $services->set(NoUnusedImportsFixer::class);
+
+    $services->set(BlankLineAfterNamespaceFixer::class);
+
+    $services->set(NativeFunctionInvocationFixer::class);
+
+    $services->set(StrictComparisonFixer::class);
+
     $services->set(ArraySyntaxFixer::class)
         ->call('configure', [[
             'syntax' => 'short',
         ]]);
 
-    // B. full sets
+
     $containerConfigurator->import(SetList::PSR_12);
 };
 
